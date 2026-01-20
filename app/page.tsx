a2f5c65336800f93e34a48c28dab6b994ed506a7
Meir Ben Shimon <Meir.Ben-Shimon@global-e.com>
2026-01-20 23:53:39 +0200

    fixing gaps

--- app/page.tsx
+++ app/page.tsx
@@ -45,41 +45,41 @@ export default function Home() {
                 <div className='space-y-3'>
                   {/* Header Row */}
                   <div className='flex items-center gap-2 text-xs text-gray-500 font-bold px-3'>
-                      <div className='w-8 text-center shrink-0'>אות</div>
+                      <div className='w-16 text-center shrink-0'>פשוט</div>
                       <div className='w-12 text-center shrink-0 border-l border-transparent pl-2'>ערך</div>
-                      <div className='w-24 text-right'>פשוט ומילוי</div>
-                      <div className='w-16 text-center shrink-0 border-l border-transparent pl-2 ml-2'>ערך</div>
-                      <div className='w-24 text-right pr-2 mr-2'>מילוי בלבד</div>
-                      <div className='w-16 text-center shrink-0 border-r border-transparent pr-2'>ערך</div>
+                      <div className='w-16 text-center'>פשוט ומילוי</div>
+                      <div className='w-12 text-center shrink-0 border-l border-transparent pl-2'>ערך</div>
+                      <div className='w-16 text-center'>מילוי</div>
+                      <div className='w-12 text-center shrink-0 border-l border-transparent pl-2'>ערך</div>
                   </div>
 
                   {breakdown.map((item, index) => (
                     <div key={index} className='flex items-center gap-2 bg-blue-50 p-3 rounded'>
                       {/* 1. Letter */}
-                      <div className='w-8 font-bold text-2xl text-blue-600 text-center shrink-0'>{item.char}</div>
+                      <div className='w-16 font-bold text-2xl text-blue-600 text-center shrink-0'>{item.char}</div>
                       
                       {/* 2. Basic Value */}
                       <div className='w-12 font-mono font-bold text-lg text-gray-500 text-center shrink-0 border-l border-blue-200 pl-2'>{item.basicValue}</div>
 
                       {/* 3. Full Filling Text */}
-                      <div className='w-24 flex flex-col'>
-                          <span className='font-bold text-gray-800 leading-none'>{item.full}</span>
+                      <div className='w-16 flex flex-col'>
+                          <span className='font-bold text-gray-800 text-center leading-none'>{item.full}</span>
                       </div>
 
                       {/* 4. Full Filling Value */}
-                      <div className='w-16 flex flex-col items-center shrink-0 border-l border-blue-200 pl-2 ml-2'>
+                      <div className='w-12 flex flex-col items-center shrink-0 border-l border-blue-200 pl-2'>
                           <span className='font-mono font-bold text-lg text-blue-700 leading-none'>{item.value}</span>
                       </div>
 
                       {/* 5. Partial Filling Text */}
-                      <div className='w-24 flex flex-col pr-2 mr-2'>
+                      <div className='w-16 flex flex-col'>
                           {item.suffix ? (
-                                  <span className='font-bold text-gray-800 leading-none'>{item.suffix}</span>
+                                  <span className='font-bold text-gray-800 text-center leading-none'>{item.suffix}</span>
                           ) : <div className='flex-1'></div>}
                       </div>
 
                       {/* 6. Partial Filling Value */}
-                      <div className='w-16 flex flex-col items-center shrink-0 border-r border-blue-200 pr-2'>
+                      <div className='w-12 flex flex-col items-center shrink-0 border-l border-transparent pl-2'>
                           {item.suffix && (
                                   <span className='font-mono font-bold text-green-600 leading-none'>{item.suffixValue}</span>
                           )}
@@ -90,7 +90,7 @@ export default function Home() {
 
                 <div className='mt-3 flex items-center gap-2 bg-gray-200 p-3 rounded-lg font-bold'>
                   {/* 1. Label (in place of Letter column spacer) */}
-                  <div className='w-8 shrink-0 text-right text-gray-700 text-sm'>סה"כ:</div>
+                  <div className='w-16 shrink-0 text-right text-gray-700 text-sm'>סה"כ:</div>
                   
                   {/* 2. Total Basic Value */}
                   <div className='w-12 text-center shrink-0 text-gray-600 border-l border-gray-400 pl-2'>
@@ -98,18 +98,18 @@ export default function Home() {
                   </div>
 
                   {/* 3. Spacer (Full Filling Text column) */}
-                  <div className='w-24'></div>
+                  <div className='w-16'></div>
 
                   {/* 4. Total Full Value */}
-                  <div className='w-16 flex flex-col items-center shrink-0 border-l border-gray-400 pl-2 ml-2'>
+                  <div className='w-12 flex flex-col items-center shrink-0 border-l border-gray-400 pl-2'>
                       <span className='text-xl text-blue-900 leading-none'>{totalValue}</span>
                   </div>
 
                   {/* 5. Spacer */}
-                  <div className='w-24 pr-2 mr-2'></div>
+                  <div className='w-16'></div>
 
                   {/* 6. Total Partial Value */}
-                  <div className='w-16 flex flex-col items-center shrink-0 border-r border-gray-400 pr-2'>
+                  <div className='w-12 flex flex-col items-center shrink-0 border-l border-transparent pl-2'>
                       <span className='text-lg text-green-800 leading-none'>{totalSuffixValue}</span>
                   </div>
                 </div>
